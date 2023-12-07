4d4bc7a05f26b2721af77db6ddcc4904d68b9077
Chiara Muller <chiaramuller@hotmail.be>
2023-12-07 18:44:15 +0100

    F02: ROUTES > Create order and error route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LoginPage from './components/pages/login/LoginPage'
+import OrderPage from './components/pages/order/OrderPage'
+import ErrorPage from './components/pages/error/ErrorPage'
 
 function App() {
 
   return (
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
   )
 }
