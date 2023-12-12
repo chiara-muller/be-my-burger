71472a35daa3fb4c9ad878bb475ba5d849c4265d
Chiara Muller <chiaramuller@hotmail.be>
2023-12-12 18:01:03 +0100

    F03: LoginPage background + Front Form

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components'
 
 export default function LoginForm() {
 
@@ -18,11 +19,11 @@ export default function LoginForm() {
     }
 
   return (
-    <div>
-      <form action="submit" onSubmit={handleSubmit}>
+    <div className="form-container">
+      <LoginFormStyled action="submit" onSubmit={handleSubmit}>
         <h2>Bienvenue chez nous !</h2>
         <br />
-        <h4>Connectez-vous</h4>
+        <h2>Connectez-vous</h2>
         <input
           value={inputValue}
           onChange={handleChange}
@@ -30,8 +31,48 @@ export default function LoginForm() {
           placeholder="Entrez votre prénom..."
           required
         />
-        <button>Accédez à votre espace</button>
-      </form>
+        <button> Accédez à votre espace</button>
+      </LoginFormStyled>
     </div>
   )
 }
+
+const LoginFormStyled = styled.form `
+
+  .form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
+  }
+
+  input {
+    width: 400px;
+    height: 55px;
+    top: 272.31px;
+    left: 32px;
+    padding: 18px, 24px, 18px, 24px;
+    border-radius: 5px;
+    gap: 12.8px;
+  }
+
+  button {
+    color: white;
+    width: 400px;
+    height: 53px;
+    top: 345.31px;
+    left: 32px;
+    padding: 18px, 106.2px, 18px, 106.2px;
+    border-radius: 5px;
+    border: 1px;
+    background: #FF9F1B;
+    border: 1px solid #FF9F1B;
+    font-family: Arial;
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 15px;
+    letter-spacing: 0em;
+    text-align: center;
+  }
+`
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,10 +1,28 @@
-
 import LoginForm from "./LoginForm"
+import styled from "styled-components"
+
 
 export default function LoginPage() {
 
   return (
-     <LoginForm />
-)
+    <LoginPageStyled>
+
+        <LoginForm />
+
+    </LoginPageStyled>
+  )
 
 }
+
+const LoginPageStyled = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background-image: url("F03 assets/F03 burger-background.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  box-shadow: 0px 4px 4px 0px #00000040;
+`;
