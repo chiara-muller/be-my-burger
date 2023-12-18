d8e160b2bf5f80208af0d4ebebdaf45c00d09315
Chiara Muller <chiaramuller@hotmail.be>
2023-12-18 14:02:58 +0100

    F04: OrderPage > background main div ok

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,4 +1,5 @@
 // import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
 import NavBar from "./NavBar";
 
 export default function OrderPage() {
@@ -7,11 +8,25 @@ export default function OrderPage() {
 
   return (
     <div>
-      <NavBar />
-      {/* <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link> */}
+      <div>
+        <NavBar />
+        {/* <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Deconnexion</button>
+        </Link> */}
+      </div>
+      <OrderPageStyled>
+
+      </OrderPageStyled>
     </div>
   )
 }
+
+const OrderPageStyled = styled.div`
+
+  height: 100vh;
+  background: #F5F5F7;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+  border-radius: 0px 0px 15px 15px;
+
+`;
