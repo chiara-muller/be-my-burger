38f72db8520f7f6c7d12992bee5586928bd1805b
Chiara Muller <chiaramuller@hotmail.be>
2023-12-19 17:52:39 +0100

    F05: MENU > Cards layout + added data

--- src/components/pages/order/Card.jsx
+++ src/components/pages/order/Card.jsx
@@ -1,27 +1,47 @@
 import styled from "styled-components";
 import { fakeMenu1 } from "../../fakeMenu";
+import PrimaryButton from "../../resusable-ui/PrimaryButton"
 
 export default function Card() {
+
   return (
     <CardStyled>
       {fakeMenu1.map((menu) => {
         return (
-
-            <p key={menu.id}>{menu.title}</p>
-
+          <div key={menu.id} className="cards">
+            <img src={menu.imageSource} alt="menu image"/>
+            <h4 key={menu.id}>{menu.title}</h4>
+            <div className="bottom-card">
+              <p>{menu.price}</p>
+              <PrimaryButton label={"Ajouter"}/>
+            </div>
+          </div>
           )
-        })}
+      })}
     </CardStyled>
     )
   }
 
 const CardStyled = styled.div`
-  background-color: red;
-  width: 240px;
-  height: 330px;
-  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
 
-  .menu {
-    background-color: green;
+  display: flex;
+
+  .cards {
+    background-color: red;
+    width: 240px;
+    height: 330px;
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+
+    img {
+      width: 50%;
+    }
+
+    .bottom-card {
+      display: flex;
+      justify-content: space-between;
+
+    }
   }
+
+
 `;
