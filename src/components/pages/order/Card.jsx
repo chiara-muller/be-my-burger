d46b2d027b20560e4ff92dfc6d5709c0e887cc74
Chiara Muller <chiaramuller@hotmail.be>
2023-12-20 14:23:25 +0100

    F05: MENU > Cards photo size ok

--- src/components/pages/order/Card.jsx
+++ src/components/pages/order/Card.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
-import { fakeMenu1 } from "../../fakeMenu";
+import { fakeMenu2 } from "../../fakeMenu";
 import PrimaryButton from "../../resusable-ui/PrimaryButton"
 
 export default function Card() {
 
   return (
     <CardStyled>
-      {fakeMenu1.map((menu) => {
+      {fakeMenu2.map((menu) => {
         return (
           <div key={menu.id} className="cards">
             <img src={menu.imageSource} alt="menu image"/>
-            <h4 key={menu.id}>{menu.title}</h4>
+            <h4 key={menu.id} className="item-name">{menu.title}</h4>
             <div className="bottom-card">
               <p>{menu.price}</p>
               <PrimaryButton label={"Ajouter"}/>
@@ -24,22 +24,40 @@ export default function Card() {
 
 const CardStyled = styled.div`
 
-  display: flex;
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 40px;
+  grid-auto-rows: minmax(100px, auto);
+
 
   .cards {
-    background-color: red;
+
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
     width: 240px;
     height: 330px;
     box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
 
     img {
-      width: 50%;
+      grid-row: 2/4;
+      max-width: 200px;
+      max-height: 140px;;
+      padding-top: 40px;
+    }
+
+    .item-name {
+      grid-row: 3;
+      font-family: "Amatic SC", cursive;
+      text-align: left;
+      font-size: 28px;
     }
 
     .bottom-card {
+      grid-row: 4;
       display: flex;
       justify-content: space-between;
-
+      align-items: center;
     }
   }
 
