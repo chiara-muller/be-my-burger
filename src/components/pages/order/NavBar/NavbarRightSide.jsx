5193a363393f78ca8770a82ab6981b7f9e1313fc
Chiara Muller <chiaramuller@hotmail.be>
2023-12-22 19:26:39 +0100

    F06: NAVBAR > add toast on admin button click

--- src/components/pages/order/NavBar/NavbarRightSide.jsx
+++ src/components/pages/order/NavBar/NavbarRightSide.jsx
@@ -1,13 +1,44 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../resusable-ui/ToggleButton";
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer, toast } from "react-toastify"
+import { theme } from "../../../../theme";
+import { useState } from "react";
 
 
 export default function NavbarRightSide() {
 
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+
+
+
+  const notify = () => {
+
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+      // icon: <FaUserSecret size={30} />,
+      theme: "dark",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      })
+    }
+    setIsModeAdmin(!isModeAdmin)
+  }
+
   return (
     <NavbarRightSideStyled>
-      <ToggleButton labelIfUnchecked="DESACTIVER LE MODE ADMIN" labelIfChecked="ACTIVER LE MODE ADMIN" />
+      <ToggleButton
+        labelIfUnchecked="Activer le mode admin"
+        labelIfChecked="Désactiver le mode admin"
+        onToggle={notify}
+      />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
       <Profile />
     </NavbarRightSideStyled>
   )
@@ -18,5 +49,23 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 
 `;
