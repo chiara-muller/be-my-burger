de1c19ff869f720d8c6fea59af3b2bb947f9b22d
Chiara Muller <chiaramuller@hotmail.be>
2023-12-29 13:01:27 +0100

    F07: Update theme file

--- src/components/pages/order/Main/AdminPanel.jsx
+++ src/components/pages/order/Main/AdminPanel.jsx
@@ -1,33 +1,73 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
+import PanelContext from "../../../../context/PanelContext";
 
 export default function AdminPanel() {
 
-  const [ showPanel, setShowPanel ] = useState(false)
+  const [ isPanelVisible, setIsPanelVisible ] = useState(false)
 
-  const handleClick = () => {
-    setShowPanel(!showPanel)
+  const panelContextValue = {
+    isPanelVisible,
+    setIsPanelVisible
   }
+
+  // const handleClick = () => {
+  //   setShowPanel(!showPanel)
+  // }
 
   return (
     <AdminPanelStyled>
       <div className="button-container">
-        <button onClick={handleClick}>
-          {showPanel ? 'up' : 'down'}
-        </button>
-        <button>Ajouter un produit</button>
-        <button>Modifier un produit</button>
-          {showPanel && (
-            <div className="content-container">
-              contenu
-            </div>
-          )}
+        <PanelContext.Provider value={panelContextValue}>
+        <Button />
+        </PanelContext.Provider>
       </div>
-
     </AdminPanelStyled>
   )
   }
 
+
+function Button() {
+  return (
+    <div>
+      <button>Toggle</button>
+      <Button1 />
+    </div>
+  )
+}
+
+function Button1() {
+  return (
+    <div>
+      <button>
+        Ajouter un produit
+      </button>
+      <Button2 />
+    </div>
+  )
+}
+
+function Button2() {
+
+  const [ panelVisible, setPanelVisible ] = useState(false)
+  const { isPanelVisible, setIsPanelVisible } = useContext(PanelContext);
+
+  const handleClick = () => {
+    setIsPanelVisible(!panelVisible)
+    panelVisible
+}
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Modifier un produit
+      </button>
+      {isPanelVisible && <div className="content-container">
+        contenu
+      </div>}
+    </div>
+    )
+}
 
 const AdminPanelStyled = styled.div`
 
---
+++ src/context/PanelContext.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext } from "react";
+
+export default createContext({
+  isPanelVisible: false,
+  setIsPanelVisible: Boolean
+})
