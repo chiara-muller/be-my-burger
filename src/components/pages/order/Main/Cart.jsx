230c4782fe207fee52fa28fdafd79805783db1c4
Chiara Muller <chiaramuller@hotmail.be>
2024-01-11 18:34:20 +0100

    F10: CART > add styling

--- src/components/pages/order/Main/Cart.jsx
+++ src/components/pages/order/Main/Cart.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { theme } from "../../../../theme"
 
 export default function Cart() {
   return (
     <CartStyled>
-      <div className="total-container">total</div>
-      <div className="order-container">commande</div>
-      <div className="footer-container">footer</div>
+      <div className="total-container">
+        <p>Total</p>
+        <p>0,00€</p>
+      </div>
+      <div className="order-container">Votre commande est vide.</div>
+      <div className="footer-container">Codé avec ❤️ et React.JS</div>
     </CartStyled>
   )
 }
@@ -13,31 +17,43 @@ export default function Cart() {
 const CartStyled = styled.div`
 
   box-sizing: border-box;
-  border: 2px solid blue;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  box-shadow: 0px 0px 20px 0px #00000033 inset;
 
   .total-container {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    border: 1px solid red;
-    height: 70px;
+    padding-left: 12px;
+    padding-right: 12px;
+    height: 10%;
+    background-color: ${theme.colors.dark};
+    color: ${theme.colors.primary};
+    font-family: ${theme.fonts.family.stylish};
+    font-size: ${theme.fonts.size.P4};
   }
   .order-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    border: 1px solid green;
-    height: 100%;
+    height: 80%;
+    color: ${theme.colors.greyBlue};
+    font-family: ${theme.fonts.family.stylish};
+    font-size: ${theme.fonts.size.P4};
   }
   .footer-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    border: 1px solid purple;
-    height: 70px;
+    padding-left: 12px;
+    padding-right: 12px;
+    height: 10%;
+    background-color: ${theme.colors.dark};
+    color: ${theme.colors.white};
+    font-family: ${theme.fonts.family.stylish};
+    font-size: ${theme.fonts.size.P2};
   }
 
 `;
