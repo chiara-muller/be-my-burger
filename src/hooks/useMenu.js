58adef8c4962f2b1145a58481119a8afc00f5300
Chiara Muller <chiaramuller@hotmail.be>
2024-01-12 17:23:12 +0100

    F10: ORDER > OrderPage > create custom hook useMenu for menu Items

---
+++ src/hooks/useMenu.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { fakeMenu } from "../fakeData/fakeMenu"
+import { deepClone } from "../utils/array"
+
+export function useMenu() {
+
+  const [ menu, setMenu ] = useState(fakeMenu.SMALL)
+
+  const handleAdd = (itemToAdd) => {
+    const menuCopy = deepClone(menu) // deep clone of the menu
+    // or const menuCopy = [...menu]
+    menuCopy.push(itemToAdd)
+    setMenu(menuCopy)
+  }
+
+  const handleDelete = (itemId) => {
+    const menuCopy = deepClone(menu)
+    const newMenu = menuCopy.filter((item) => item.id !== itemId)
+    setMenu(newMenu)
+  }
+
+  const handleEdit = (itemBeingEdited) => {
+    const menuCopy = deepClone(menu)
+    const idOfItemToEdit = menuCopy.findIndex((item) => item.id === itemBeingEdited.id)
+    console.log(idOfItemToEdit)
+    menuCopy[idOfItemToEdit] = itemBeingEdited
+     // permet de voir les modification en temps réel grace a itemBeingUpdated dans la fonction handleChange
+     setMenu(menuCopy)
+  }
+
+  const resetMenu = () => {
+    setMenu(fakeMenu.LARGE)
+  }
+
+  return { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu}
+}
