3055adc31a430748f6390710129ffb401160e5db
Chiara Muller <chiaramuller@hotmail.be>
2024-01-24 13:19:18 +0100

    F11: CART > add delete button in cartItem

--- src/components/pages/order/Main/Cart/CartItem.jsx
+++ src/components/pages/order/Main/Cart/CartItem.jsx
@@ -4,31 +4,57 @@ export default function CartItem({title, id, imageSource, price, quantity, onDel
 
   return (
    <CartItemStyled key={id}>
-    <div className="img"><img src={imageSource} alt="image" /></div>
+    <div className="img-container">
+      <img src={imageSource} alt="image" />
+    </div>
     <div className="info">
       <p>{title}</p>
       <p>{price}</p>
     </div>
     <div className="quantity">{quantity}</div>
-    <button onClick={onDeleteItem}>delete</button>
+    <button className="delete-button" onClick={onDeleteItem}>delete</button>
    </CartItemStyled>
   )
 }
 
 const CartItemStyled = styled.div`
-  border: 2px solid red;
+
+  max-height: 85px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 
-  .img {
+  .img-container {
+    border: 1px solid blue;
     max-width: 85px;
     max-height: 70px;
 
     img {
       width: 100%;
       height: 100%;
-      object-fit: contain;
+      object-fit: cover;
+    }
+  }
+
+  .delete-button {
+    width: 75px;
+    height: 85px;
+    display: none;
+  }
+
+  .quantity {
+    width: 75px;
+    height: 85px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      visibility: hidden;
+    }
+
+    &:hover + .delete-button {
+      display: block;
     }
   }
 
